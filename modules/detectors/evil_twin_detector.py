55264f5c16e5124a93f033567351f7c77466613a
anotherik <ricardojotagoncalves@hotmail.com>
2017-02-19 20:12:34 +0000

    base detector module

--- modules/detectors/evil_twin_detector.py
+++ modules/detectors/evil_twin_detector.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 # author: anotherik (Ricardo Gonçalves)
 
+# the method will return in case of a detection
+def rogueAP_detector(access_point, captured_aps):
+	rogue = False
+	for ap in captured_aps:
+		try:
+			if ap['essid'] == access_point['essid'] and ap['mac'] != access_point['mac']: # type of rap (same ssid and dif bssid)
+				rogue = True
+			if ap['mac'] == access_point['mac'] and ap['essid'] != access_point['essid']: # type of rap (same bssid and dif ssid)
+				rogue = True
+			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] != access_point['channel']: # type of rap (same ssid, same bssid and dif channel)
+				rogue = True
+		except Exception as e:
+			print e
+			pass
+	return rogue
