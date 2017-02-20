873f6d83983d0792009525ddc39bdd163aeaa743
anotherik <ricardojotagoncalves@hotmail.com>
2017-02-20 20:43:23 +0000

    database api

--- data/manipulate_db.py
+++ data/manipulate_db.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python2
+# -*- coding: utf-8 -*-
 # author: anotherik (Ricardo Gonçalves)
 
 import sqlite3
 
 # create/open a (new) database inside the folder data
-def open_db(dbname):
-	conn = sqlite3.connect('data/'+dbname+'.db')
-	print("Opened database successfully")
+def open_db(): #dbname
+	conn = sqlite3.connect(":memory:") #'data/'+dbname+'.db'
+	#print("Opened database successfully")
 	return conn
 
 # create a table inside the chosen database for the scanned Access Points (APs)
-def create_table(conn, dbname):
+def create_table(conn): #, dbname
 	conn.execute('''CREATE TABLE APs
        (ID INT PRIMARY KEY     NOT NULL,
        SSID           CHAR(50),
@@ -24,14 +25,37 @@ def create_table(conn, dbname):
        Authentication CHAR(50),
        Manufacturer   CHAR(50));''')
 
-	print("APs table created successfully")
-	conn.close()
+	#print("APs table created successfully")
 
+
+# create a table inside the chosen database for the scanned Access Points (APs)
+def create_table_scapy(conn): #, dbname
+       conn.execute('''CREATE TABLE APs
+       (ssid           CHAR(50),
+       bssid          CHAR(50),
+       channel        INT,
+       manufacturer   CHAR(50),
+       encryption     CHAR(50));''')
+
+       #print("Scapy APs table created successfully")
 
 # insert values in the table APs of the chosen database
 def insert_in_db(conn, ssid, bssid, ch, signal, quality, encryption, cipher, pairwise, authentication, manufacturer):
 	conn.execute("insert into APs (ssid, bssid, ch, signal, quality, encryption, cipher, pairwise, authentication, manufacturer) values (?,?,?,?,?,?,?,?,?,?)", (ssid, bssid, ch, signal, quality, encryption, cipher, pairwise, authentication, manufacturer))
 	conn.commit()
+
+# insert values in the table APs of the chosen database
+def insert_in_db_scapy(conn, ssid, bssid, ch, manufacturer, encryption):
+       conn.execute("insert into APs (ssid, bssid, channel, manufacturer, encryption) values (?,?,?,?,?)", (ssid, bssid, ch, manufacturer, encryption))
+       conn.commit()
+
+def select_from_db(conn):
+       cur = conn.cursor()
+       cur.execute("select * from APs")
+       for row in cur:
+              print row
+
+       conn.close()
 
 #db_name = raw_input("Enter the name for the database: ")
 #open_db(db_name)
