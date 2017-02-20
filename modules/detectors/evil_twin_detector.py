3c63c433994dc72d5d3e332906958f51031225cc
anotherik <ricardojotagoncalves@hotmail.com>
2017-02-20 23:20:32 +0000

    more one detection case

--- modules/detectors/evil_twin_detector.py
+++ modules/detectors/evil_twin_detector.py
@@ -7,11 +7,17 @@ def rogueAP_detector(access_point, captured_aps):
 	rogue = False
 	for ap in captured_aps:
 		try:
-			if ap['essid'] == access_point['essid'] and ap['mac'] != access_point['mac']: # type of rap (same ssid and dif bssid)
+			# type of rap (same ssid and dif bssid)
+			if ap['essid'] == access_point['essid'] and ap['mac'] != access_point['mac']:
 				rogue = True
-			if ap['mac'] == access_point['mac'] and ap['essid'] != access_point['essid']: # type of rap (same bssid and dif ssid)
+			# type of rap (same bssid and dif ssid)
+			if ap['mac'] == access_point['mac'] and ap['essid'] != access_point['essid']:
 				rogue = True
-			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] != access_point['channel']: # type of rap (same ssid, same bssid and dif channel)
+			# type of rap (same ssid, same bssid and dif channel)
+			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] != access_point['channel']:
+				rogue = True
+			# type of rap (same ssid, same bssid and same channel)
+			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] == access_point['channel']:
 				rogue = True
 		except Exception as e:
 			print e
