d95bad658c29cc92eed4ba80036322191cce8e06
anotherik <ricardojotagoncalves@hotmail.com>
2017-09-18 01:11:05 +0100

    scanners module updated

--- modules/scanners/iwlist_network_monitor.py
+++ modules/scanners/iwlist_network_monitor.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python2
 # Network monitor using iwlist
-import subprocess, sys, time, re, json, os, Queue
+import subprocess, sys, time, re, json, os, signal
 import manufacturer.parse_manufacturer as manufacturer
 import modules.detectors.evil_twin_detector as detector1
+import modules.logs.logs_api as logs_api
+import modules.detectors.passive_detectors as passive_detectors
 
 captured_aps = []
 manufacturer_table = "manufacturer/manufacturer_table.txt"
 table_of_manufacturers = {}
+
+def getTimeDate():
+	return time.strftime("%X") +" "+ time.strftime("%x")#time.strftime("%c")
 
 class colors:
         HEADER = '\033[95m'
@@ -21,13 +26,19 @@ class colors:
         GRAY = '\033[90m'
         UNDERLINE = '\033[4m'
 
-def scan(interface, queue):
+def signal_handler(signal, frame):
+	print colors.GRAY + "\nYou pressed Ctrl+C!\nGoodbye!" + colors.ENDC
+	#detector1.statistics()
+	sys.exit(0)
 
+def scan(*arg):
+	interface = arg[0]
+	profile = arg[1]
 	global table_of_manufacturers
 	table_of_manufacturers = manufacturer.MacParser(manufacturer_table).refresh()
 
-	table = ['AP Name','BSSID', 'CH', 'Brand','Signal','Quality','Encryption','Cipher', 'Pairwise','Authentication']
-	print colors.WARNING + '{:^22s}|{:^19s}|{:^9s}|{:^24s}|{:^8s}|{:^9s}|{:^16s}|{:^8s}|{:^11s}|{:^16s}'.format(table[0],table[1],table[2],table[3],table[4],table[5],table[6],table[7],table[8],table[9]) + colors.ENDC
+	table = ['Date','AP Name','BSSID', 'CH', 'Brand','Signal','Quality','Encryption','Cipher', 'Pairwise','Authentication']
+	print colors.WARNING + '{:^25s}|{:^22s}|{:^19s}|{:^9s}|{:^24s}|{:^8s}|{:^9s}|{:^16s}|{:^8s}|{:^11s}|{:^16s}'.format(table[0],table[1],table[2],table[3],table[4],table[5],table[6],table[7],table[8],table[9],table[10]) + colors.ENDC
 	while True:
 		ap_list = get_results(interface)
 		#print ap_list
@@ -40,24 +51,25 @@ def scan(interface, queue):
 					if len(line['essid'])>21:
 						limited = True
 					
+					passive_detectors.authorized_aps_iwlist(line, profile)	
+
 					if limited:
 						if detector1.rogueAP_detector(line,captured_aps):
-							print colors.FAIL2 + '{:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(line['essid'][0:21],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites']) + colors.ENDC
+							print colors.FAIL2 + '{:^25s} {:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(getTimeDate(),line['essid'][0:21],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites']) + colors.ENDC
 						else:
-							print '{:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(line['essid'][0:21],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites'])
+							print '{:^25s} {:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(getTimeDate(),line['essid'][0:21],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites'])
 					else:
 						if detector1.rogueAP_detector(line,captured_aps):
-							print colors.FAIL2 + '{:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(line['essid'],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites']) + colors.ENDC
+							print colors.FAIL2 + '{:^25s} {:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(getTimeDate(),line['essid'],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites']) + colors.ENDC
 						else:
-							print '{:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(line['essid'],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites'])
+							print '{:^25s} {:<21s}  {:^19s} {:^9s} {:^24s} {:^8s} {:^9s} {:^16s} {:^8s}  {:^10s} {:^16s}'.format(getTimeDate(),line['essid'],line['mac'],line['channel'], line['manufacturer'],line['signal'],line['quality'],line['key type'],line['group cipher'],line['pairwise cipher'], line['authentication suites'])
 
 					captured_aps.append(line)
 					#print captured_aps
-					queue.put(line)
-					
-
+			signal.signal(signal.SIGINT, signal_handler)
 			time.sleep(1)
-		except:
+		except Exception, err:
+			logs_api.errors_log(str(err))
 			pass
 
 def get_results(interface):
@@ -68,7 +80,7 @@ def get_results(interface):
 		#Break the output making an array containing the info of each Access Point 
         list_of_results = re.split(r'\bCell \d{2}\b - ',proc)[1:]
     except subprocess.CalledProcessError:
- 		print 'Error...'
+    	logs_api.errors_log("Error"+str(subprocess.CalledProcessError))
 
     return parse(list_of_results)
 
@@ -78,10 +90,14 @@ def filter_aps(access_point):
 			#print "a= " + str(float(ap['quality_calc']))
 			#print "b= " + str(float(access_point['quality_calc']))
 			#print "diff =" + str( abs((float(ap['quality_calc'])) - (float(access_point['quality_calc']))) )
-			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] == access_point['channel'] and ap['key type'] == access_point['key type'] and ap['group cipher'] == access_point['group cipher'] and ( abs( float(ap['quality_calc']) - float(access_point['quality_calc']) ) <= 0.25 ):
+			#if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] == access_point['channel'] and ap['key type'] == access_point['key type'] and ap['group cipher'] == access_point['group cipher'] and ( abs( float(ap['quality_calc']) - float(access_point['quality_calc']) ) <= 0.25 ):
+			#print "***************************************************"
+			#print abs(int(access_point['signal']))
+			#print abs(int(ap['signal']))
+			if ap['essid'] == access_point['essid'] and ap['mac'] == access_point['mac'] and ap['channel'] == access_point['channel'] and ap['key type'] == access_point['key type'] and ap['group cipher'] == access_point['group cipher'] and ( abs(int(access_point['signal'])) <= abs(int(ap['signal']))+15 and abs(int(access_point['signal'])) >= abs(int(ap['signal']))-15):
 				return False
 		except Exception as e: 
-			print "Exception found: \n"+e
+			logs_api.errors_log("Exception found: "+str(e))
 			pass		
     return True
 
--- modules/scanners/scapy_network_monitor.py
+++ modules/scanners/scapy_network_monitor.py
@@ -8,10 +8,13 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 from scapy.all import *
+from multiprocessing import Queue, Process
 import os, signal, sys, string
 import manufacturer.parse_manufacturer as manufacturer
-import modules.detectors.evil_twin_detector as detector1
 import data.manipulate_db as db_api
+import modules.manage_interfaces as manage_interfaces
+import modules.colors as colors
+import modules.detectors.passive_detectors as passive_detectors
 
 manufacturer_table = "manufacturer/manufacturer_table.txt"
 table_of_manufacturers = {}
@@ -21,30 +24,9 @@ encryption = "0"
 vendor = ""
 spaces = 0
 
-global current_ch
-current_ch = 6
-
-class colors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WHITE = '\033[37m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	GRAY = '\033[90m'
-	UNDERLINE = '\033[4m'
-
 def aps_lookup(pkt):
-	global table_of_manufacturers, current_ch
+	global table_of_manufacturers
 	table_of_manufacturers = manufacturer.MacParser(manufacturer_table).refresh()
-
-	if(current_ch >= 13):
-		current_ch = 1
-	os.system("iw dev %s set channel %d" % (interface, current_ch) )
-	#os.system("iwconfig %s channel %s" % (interface, current_ch) )
-	#time.sleep(0.1)
 
 	# we are checking if ssid is already in the access_points list (and we also want same ssid with different bssid)
 	if ( (pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp)) and (pkt[Dot11].addr3 not in access_points) ):
@@ -56,51 +38,77 @@ def aps_lookup(pkt):
 		capability = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}\
 		        {Dot11ProbeResp:%Dot11ProbeResp.cap%}")
 
+		extra = pkt.notdecoded
+		sig_str = -(256-ord(extra[-4:-3]))
+
 		manufacturer_data = manufacturer.search(table_of_manufacturers,str(pkt.addr2))
 		if(manufacturer_data == []):
 			vendor = "Not Found"
 		else:
-			vendor = manufacturer_data[0].comment
+			vendor = manufacturer_data[0].manuf
+
+		if(str(vendor) == "None"):
+			vendor = "Not Found"	
 
 		if re.search("privacy", capability): encryption = "1"
 		else: encryption = "0"
 
+		# call passive detectors
+		passive_detectors.authorized_aps(ssid, bssid, sig_str, encryption, profile)
+
 		spaces = 23 - len(ssid)
 		spaces = ' '*spaces
-		spaces2 = 26 - len(vendor)
-		spaces2 = ' '*spaces2
+		#spaces2 = 18 - len(vendor)
+		#spaces2 = ' '*spaces2
 		if encryption=="0":
-			print colors.OKGREEN+"%s%s%s  %2d  %s%s%s" % (ssid, spaces, bssid, int(channel), vendor, spaces2, encryption) +colors.ENDC
+			print colors.get_color("OKGREEN")+"%s %s %s %2d %s   %s  %s" % (ssid, spaces, bssid, int(channel), vendor, encryption, sig_str) + colors.get_color("ENDC")
 		else:	
-			print "%s%s%s  %2d  %s%s%s" % (ssid, spaces, bssid, int(channel), vendor, spaces2, encryption)
+			print "%s %s %s %2d %s   %s  %s" % (ssid, spaces, bssid, int(channel), vendor, encryption, sig_str)
 		db_api.insert_in_db_scapy(conn, ssid, bssid, int(channel), vendor, encryption)
 
-	current_ch+=1
 	signal.signal(signal.SIGINT, signal_handler)
 
-# disable monitor mode to the given interface
-def disable_monitor():
-	print("\nChanging "+str(interface)+" to managed mode.")
-	os.system("ifconfig %s down" % interface)
-	os.system("iwconfig %s mode managed" % interface)
-	os.system("ifconfig %s up" % interface)
+
+def channel_hopper():
+	current_ch = 6
+	while True:
+		try:
+			current_ch+=1
+			if(current_ch > 13):
+				current_ch = 1
+			#print("The current_ch: %s" % str(current_ch))	
+			os.system("iw dev %s set channel %d" % (interface, current_ch) )
+			time.sleep(0.5)
+		except KeyboardInterrupt:
+			break
 
 def signal_handler(signal, frame):
 	db_api.select_from_db(conn)
-	disable_monitor()
+	manage_interfaces.disable_monitor(interface)
 	print("Goodbye! ")
 	sys.exit(0)
 
-
 def printHeader():
-	print colors.WARNING + "SSID                   BSSID              CH  BRAND                     ENCRYPTION" + colors.ENDC
+	print(colors.get_color("WARNING") + "SSID\t\t\t\tBSSID\t   CH  BRAND\tENC  RSSI" + colors.get_color("ENDC"))
 
-def scapy_scan(i):
-	global interface
-	interface = i
+def scapy_scan(*arg):
+	global interface, profile
+	interface = arg[0]
+	if (len(arg)>1):
+		profile = arg[1]
 	printHeader()
 	global conn
 	conn = db_api.open_db()
 	conn.text_factory = str
 	db_api.create_table_scapy(conn)
-	sniff(iface=interface, prn=aps_lookup)	
+
+	# start the channel hopper
+	p = Process(target = channel_hopper)
+	p.start()
+
+    # start scanning
+	sniff(iface=interface, prn=aps_lookup)
+
+	p.terminate()
+	p.join()
+	sys.exit(0)
