c2143ce960a1c77fb64e27309cb839153058d3d9
anotherik <ricardojotagoncalves@hotmail.com>
2017-09-21 02:01:35 +0100

    passive detectors improved

--- modules/detectors/passive_detectors.py
+++ modules/detectors/passive_detectors.py
@@ -41,12 +41,14 @@ def authorized_aps_iwlist(scanned_ap, profile):
 				 	auth_bssids.append(line.split()[c])
 				 	c+=1
 
-				if (scanned_ap['mac'].lower() in auth_bssids):
+				#print ("scanned ap: %s" % scanned_ap['mac'])
+				#print ("auth bssids: %s" % auth_bssids) 	
+				if (scanned_ap['mac'] in auth_bssids): #(.lower())
 					if (auth_enc != 'Open' and scanned_ap['key type'] == "Open"):
 						print(colors.get_color("FAIL")+"[%s | %s] Possible Rogue Access Point!\n[Type] Evil Twin, different encryption." % (scanned_ap['essid'],scanned_ap['mac']) +colors.get_color("ENDC"))
 						break
 					if ( abs(int(scanned_ap['signal'])) > auth_rssi+15 or abs(int(scanned_ap['signal'])) < auth_rssi-15 ):
-					 	print(colors.get_color("FAIL")+"[%s | %s] Stange RSSI!!! Associate? (y/n)" % (scanned_ap['essid'],scanned_ap['mac']) +colors.get_color("ENDC"))
+					 	print(colors.get_color("FAIL")+"[%s | %s] Strange RSSI!!! Associate? (y/n)" % (scanned_ap['essid'],scanned_ap['mac']) +colors.get_color("ENDC"))
 					 	associate = str(raw_input())
 					 	if(associate=="y"):
 					 		pwd = str(raw_input("Enter AP password: "))
@@ -56,3 +58,31 @@ def authorized_aps_iwlist(scanned_ap, profile):
 					 		break
 				else:
 					print(colors.get_color("FAIL")+"[%s | %s] Possible Rogue Access Point!\n[Type] Evil Twin, unauthorized bssid." % (scanned_ap['essid'],scanned_ap['mac']) +colors.get_color("ENDC") )	
+
+
+phishing_karma = {}
+def spot_karma(scanned_ap):
+	#print("Looking fot karmas...")
+	
+	#print phishing_karma
+	#print (scanned_ap['mac'] in phishing_karma and scanned_ap['essid'] not in phishing_karma)
+
+	if (scanned_ap['mac'] in phishing_karma):
+		c = len(phishing_karma.values())
+		cp = 0
+		for i in range(c):
+		 	if (scanned_ap['essid'] in phishing_karma.values()[i]):
+		 		break
+		 	#print ("scanned_ap %s" % scanned_ap['essid'])
+		 	#print ("phishing_karma: %s" % phishing_karma.values()[i])
+		 	if (scanned_ap['essid'] not in phishing_karma.values()[i]):
+		 		cp+=1
+		 		#print "cp added up!"
+		 	#print ("%s and %s" %(cp,c))	
+		 	if (cp == c):
+		 		phishing_karma[scanned_ap['mac']].add(scanned_ap['essid'])
+				print(colors.get_color("FAIL")+"[%s | %s] Karma Rogue Access Point!\n[Type] Karma attack." % (scanned_ap['essid'],scanned_ap['mac']) +colors.get_color("ENDC") )
+				break
+	else:
+		#print "HERE"
+		phishing_karma[scanned_ap['mac']] = set([scanned_ap['essid']])
