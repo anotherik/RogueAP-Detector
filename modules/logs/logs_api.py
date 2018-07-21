14579065dd5aaf8d80368dee0a762d3f5b8e484f
anotherik <ricardojotagoncalves@hotmail.com>
2018-07-21 17:26:03 +0100

    logs update

--- modules/logs/logs_api.py
+++ modules/logs/logs_api.py
@@ -5,11 +5,11 @@ def getDate():
 
 def errors_log(error):
 
-	if(os.path.isfile("logs/error_log.txt")):
-		with open("logs/error_log.txt", "r") as f:
+	if(os.path.isfile("logs/errors.log")):
+		with open("logs/errors.log", "r") as f:
 			if error not in f.read():
-				with open("logs/error_log.txt", "a") as f2:
+				with open("logs/errors.log", "a") as f2:
 					f2.write(getDate()+":"+error+"\n")
 	else:
-		with open("logs/error_log.txt", "a") as f:
+		with open("logs/errors.log", "a") as f:
 			f.write(getDate()+":"+error+"\n")
